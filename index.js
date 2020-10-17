360526fe76f161f5dbb5c5906e83ad7e99de5fc9
Nate Mackay <mac17039@byui.edu>
2020-10-16 19:36:17 -0600

    change access according to user login

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ express()
       }
       console.log(user.rows);
       const params = { 'todo'  : (todo)  ?  todo.rows  : null, 
-                       'user'  : (user)  ?  user.rows  : null };
+                       'user'  : (user)  ?  user.rows  : user[0] };
 
       res.send(params);
       client.release();
