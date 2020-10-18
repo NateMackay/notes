639e419d85eb7a921ecd473e187042b8bb42f015
Nate Mackay <mac17039@byui.edu>
2020-10-17 20:57:30 -0600

    add a date to each todo item

--- index.js
+++ index.js
@@ -54,8 +54,8 @@ express()
       const client = await pool.connect();
       const user   = await client.query("SELECT fname FROM Users WHERE phone='" + req.query.phone + "';");
       // "(SELECT fname FROM Users WHERE phone='" + req.query.phone + "')" 
-      // const todo   = await client.query("INSERT INTO Todo (item, priority, date, submittedBy) VALUES ('" + req.query.item + "', " + req.query.priority + ", '" + req.query.comBy + "', '" + user.rows[0].fname + "')");
-      const todo   = await client.query("INSERT INTO Todo (item, priority, date, submittedBy) VALUES ('" + req.query.item + "', " + req.query.priority + ", '" + req.query.comBy + "', '(SELECT fname FROM Users WHERE phone='" + req.query.phone + "')')");
+      const todo   = await client.query("INSERT INTO Todo (item, priority, date, submittedBy) VALUES ('" + req.query.item + "', " + req.query.priority + ", '" + req.query.comBy + "', '" + user.rows[0].fname + "')");
+      // const todo   = await client.query("INSERT INTO Todo (item, priority, date, submittedBy) VALUES ('" + req.query.item + "', " + req.query.priority + ", '" + req.query.comBy + "', '(SELECT fname FROM Users WHERE phone='" + req.query.phone + "')')");
 
       res.redirect('https://nates-notes.herokuapp.com');
       client.release();
