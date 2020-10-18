179313e94773ecbc5d916e66bd6e837f5e71bf45
Nate Mackay <mac17039@byui.edu>
2020-10-17 20:37:47 -0600

    add a date to each todo item

--- index.js
+++ index.js
@@ -22,8 +22,10 @@ express()
       const client = await pool.connect();
       const todo   = await client.query('SELECT * FROM Todo ORDER BY priority, id');
       user   = await client.query("SELECT fname FROM Users WHERE phone='" + req.query.phone + "';");
-      // console.log(user.rows);
-      
+      console.log('user ' + user);
+      console.log('user.rows ' + user.rows);
+      console.log('user.rows[0]' + user.rows[0]);
+
       if (user.rows == "" ) {
         var user = [{id: 5, fname: 'John', access: false}, {id: 5, fname: 'John', access: false}];
       }
