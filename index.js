9758aeef911e5803cb0e211e67474c144586ecb4
Nate Mackay <mac17039@byui.edu>
2020-10-26 14:57:13 -0600

    create a new list

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views',path.join(__dirname, 'views'))
 
-  // retrieve data from the database
+  // retrieve todo data from the database
   .get('/todo', async(req,res) => {
     console.log("received request to access database");
     var user = {id: 5, fname: 'John', access: false};
@@ -97,6 +97,90 @@ express()
       res.send("Error: ", err);
     }
   })
+
+
+
+
+  // retrieve Jen todo data from the database
+  .get('/todojen', async(req,res) => {
+    console.log("received request to access jen database");
+    var user = {id: 5, fname: 'John', access: false};
+
+    try {
+      const client = await pool.connect();
+      const todo   = await client.query('SELECT * FROM TodoJen ORDER BY priority, id');
+      user   = await client.query("SELECT fname FROM Users WHERE phone='" + req.query.phone + "';");
+
+      if (user.rows == "" ) {
+        var user = [{id: 5, fname: 'John', access: false}, {id: 5, fname: 'John', access: false}];
+      }
+
+      const params = { 'todo'  : (todo)  ?  todo.rows  : null, 
+                       'user'  : (user.rows)  ?  user.rows  : user };
+
+      res.send(params);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error: " + err);
+    }
+  })
+
+  .get('/updatejen', async (req, res) => {
+    try { 
+      console.log("request to add an item to Jen's list");
+      console.log(" ");
+      console.log(req.query.comBy);
+      if (req.query.comBy == "") { 
+        var assign = new Date();
+        req.query.comBy = assign.getFullYear() + "-" + (assign.getMonth() + 2) + "-" + assign.getDate();
+        console.log("new date " + req.query.comBy);
+      }
+      const client = await pool.connect();
+      const user   = await client.query("SELECT fname FROM Users WHERE phone='" + req.query.phone + "';");
+      console.log('phone = ' + req.query.phone);
+      console.log('user.rows[0].fname= ' + user.rows[0].fname);
+
+      const todo   = await client.query("INSERT INTO TodoJen (item, priority, date, submittedBy) VALUES ('" + req.query.item + "', " + req.query.priority + ", '" + req.query.comBy + "', '" + user.rows[0].fname + "')");
+
+      res.redirect('https://nates-notes.herokuapp.com/jen.html');
+      client.release();
+    } catch (err) { 
+      console.error(err);
+      res.send("Error: ", err);
+    }
+  })
+
+  .get('/deletejen', async (req, res) => {
+    try { 
+      console.log("request to delete a Jen item");
+
+      const client = await pool.connect();
+      const todo   = await client.query("DELETE FROM TodoJen WHERE item='" + req.query.delLister + "';");
+
+      res.redirect('https://nates-notes.herokuapp.com/jen.html');
+      client.release();
+    } catch (err) { 
+      console.error(err);
+      res.send("Error: ", err);
+    }
+  })
+
+  .get('/editjen', async (req, res) => {
+    try { 
+      console.log("request to update a Jen item with a new priorty");
+
+      const client = await pool.connect();
+      const todo   = await client.query("UPDATE TodoJen SET priority=" + req.query.priority + " WHERE item='" + req.query.editLister + "';");
+
+      res.redirect('https://nates-notes.herokuapp.com.jen.html');
+      client.release();
+    } catch (err) { 
+      console.error(err);
+      res.send("Error: ", err);
+    }
+  })
+  
 
   .listen(PORT, () => console.log('Listening on ${PORT}'))
 
